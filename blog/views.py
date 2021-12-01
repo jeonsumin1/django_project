031f2995d81a3d248e41a95452a44e238801d896
Jeon sumin <sumin1236@naver.com>
2021-12-01 18:19:32 +0900

    Django Project

---
+++ blog/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .forms import PostForm
+from .models import Post
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  #데이터를 바로 저장하지 x
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm()  # 비어있는 form 을 보여줌.  # +버튼을 눌렀을때 사용자에게 입력받을 수 있는 form 형식을 보여줌.
+
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            post = form.save(commit=False)  # 데이터를 바로 저장하지 x
+            post.author = request.user
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form':form})
